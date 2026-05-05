b6e1f15c165524b1dd1579640c00e0a9bea7ce29
adksjksjfls65 <180636338@qq.com>
2026-05-05 11:39:00 +0800

    Update index.js

--- src/domain/index.js
+++ src/domain/index.js
@@ -1,3 +1,7 @@
 // src/domain/index.js
-// 这是测试脚本指定的唯一入口，我们从这里统一导出需要的 API
-export { createSudoku, createGame } from '../node_modules/@sudoku/stores/grid.js';
+export { 
+    createSudoku, 
+    createSudokuFromJSON, 
+    createGame, 
+    createGameFromJSON 
+} from '../node_modules/@sudoku/stores/grid.js';
